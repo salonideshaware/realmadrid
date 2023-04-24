2dff6296df341caec1cb3c2dc9e6058fb1c876c2
vdua <vdua@adobe.com>
2023-04-24 19:28:37 +0530

    feat: hero block
    - auto blocking the first two headings and 3 links (optional) to create a hero block.

---
+++ blocks/hero/hero.js
@@ -0,0 +1,13 @@
+export default async function decorate(block) {
+  const heroContent = block.querySelector(':scope > div > div')
+  const heroPic = block.querySelector(':scope picture');
+  if (heroPic) {
+    block.append(heroPic)
+  }
+  if (heroContent) {
+    heroContent.parentElement.remove();
+    heroContent.classList.add('hero-content');
+    block.append(heroContent);
+  }
+  return block;
+}
