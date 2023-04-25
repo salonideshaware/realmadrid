eb85bdb5be3b04fd118dfa0983a6ab9cfbd73c30
msagolj <msagolj@adobe.com>
2023-04-25 16:43:42 +0200

    fixed lint issues

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,12 +1,12 @@
 /**
  * decorates the header.
- * 
- * Placeholder for now until decision who/how/if we are going to 
+ *
+ * Placeholder for now until decision who/how/if we are going to
  * do the header ourselfs.
- * 
+ *
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
-    block.setAttribute('style','background-color: lightgrey; height: var(--nav-height);')
-    block.append('HEADER');
+  block.setAttribute('style', 'background-color: lightgrey; height: var(--nav-height);');
+  block.append('HEADER');
 }
