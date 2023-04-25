12071a20898fadba8c89633b484753a9df6f2dc7
vdua <vdua@adobe.com>
2023-04-25 18:04:28 +0530

    chore: more linting issues

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,8 +1,9 @@
 export default async function decorate(block) {
   const heroContent = block.querySelector(':scope > div > div');
   const heroPic = block.querySelector(':scope picture');
-  // eslint-disable-next-line no-unused-expressions
-  heroPic && block.append(heroPic);
+  if (heroPic) {
+    block.append(heroPic);
+  }
   if (heroContent) {
     heroContent.parentElement?.remove();
     heroContent.classList.add('hero-content');
