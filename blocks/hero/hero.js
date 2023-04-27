6aabd12d983c0d7e3d3602f8fed842ea90a359b6
Francisco Chicharro Sanz <chicharr@adobe.com>
2023-04-27 16:27:19 +0200

    11 - hero block, review styles and dependency on default button decoration

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,12 +1,24 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
 export default async function decorate(block) {
   const heroContent = block.querySelector(':scope > div > div');
   const heroPic = block.querySelector(':scope picture');
   if (heroPic) {
-    block.append(heroPic);
+    const img = heroPic.querySelector('img');
+    heroPic.remove();
+    const optimizedHeroPic = createOptimizedPicture(img.src, img.alt, false, [{ media: '(min-width: 600px)', width: '2000' }, { width: '1200' }]);
+    block.append(optimizedHeroPic);
   }
   if (heroContent) {
     if (heroContent.parentElement) {
       heroContent.parentElement.remove();
+      // Remove button class from all except first link
+      Array.from(heroContent.querySelectorAll('.button'))
+        .forEach((button, index) => {
+          if (index > 0) {
+            button.classList.remove('button');
+          }
+        });
     }
     heroContent.classList.add('hero-content');
     block.append(heroContent);
