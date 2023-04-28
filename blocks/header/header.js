bd8eb6ed7a6a09de77f43ae92a4b3c0a6d22a0be
Vitaly Tsaplin <tsaplin@adobe.com>
2023-04-28 02:03:12 +0200

    feat: add header navigation

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,3 +1,15 @@
+const DATA_URL = 'https://publish-p47754-e237356.adobeaemcloud.com/graphql/execute.json/realmadridmastersite/structurePage%3Balang=es-es';
+
+async function fetchData() {
+  try {
+    const response = await fetch(DATA_URL);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+  return null;
+}
+
 /**
  * decorates the header.
  *
@@ -7,6 +19,29 @@
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
-  block.setAttribute('style', 'background-color: lightgrey; height: var(--nav-height);');
-  block.append('HEADER');
+  console.log('decorating header');
+  block.setAttribute('style', 'height: var(--nav-height); display: flex; flex-direction: row; list-style-type: none; gap: 1rem;');
+
+  const data = await fetchData();
+
+  console.log(data.data.header.items[0]);
+
+  const image = document.createElement('img');
+  // eslint-disable-next-line no-underscore-dangle
+  image.src = data.data.header.items[0].additionalLogos[0]._publishUrl;
+  block.appendChild(image);
+
+  const ul = document.createElement('ul');
+  ul.setAttribute('style', 'display: flex; flex-direction: row; list-style-type: none; gap: 1rem;');
+  block.appendChild(ul);
+  data.data.header.items[0].additionalNavigation.forEach((nav) => {
+    const li = document.createElement('li');
+    ul.appendChild(li);
+    const link = document.createElement('a');
+    link.href = nav.url;
+    link.innerHTML = nav.title;
+    li.appendChild(link);
+  });
+
+  console.log('decorated header');
 }
