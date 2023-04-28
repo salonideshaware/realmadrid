8665183a08deeed371f92ff4eba7486ea340e9db
vdua <vdua@adobe.com>
2023-04-28 08:44:52 +0530

    incorporating review comments
    - using css vars where required
    - using createOptimizedPicture API instead of generating the img tag
    - catching exceptions when fetching json from api

--- blocks/vip-areas/vip-areas.js
+++ blocks/vip-areas/vip-areas.js
@@ -1,21 +1,33 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
 async function fetchVIPAreas() {
   const url = new URL(window.location);
 
-  const resp = await fetch(`${url.origin}/area-vip/query-index.json`);
-  const json = await resp.json();
-  return json.data;
+  try {
+    const resp = await fetch(`${url.origin}/area-vip/query-index.json`);
+    const json = await resp.json();
+    return json.data;
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(`unable to fetch vip areas ${e}`);
+  }
+  return [];
 }
 
-function areaTemplate(area) {
-  return `
+function areaElement(area) {
+  const picture = createOptimizedPicture(area.image, area.title, false);
+  const li = document.createElement('li');
+  li.innerHTML = `
     <a href="${area.path}">
-      <img src="${area.image}" alt="${area.title}"/>
       <div class="info">
         <h3>${area.title}</h3>
         <p>${area.description}</p>
       </div>
     </a>
   `;
+  const anchor = li.querySelector('a');
+  anchor.insertAdjacentElement('afterbegin', picture);
+  return li;
 }
 
 /**
@@ -23,17 +35,10 @@ function areaTemplate(area) {
  * @param {Element} block The VIP areas element
  */
 export default async function decorate(block) {
-  function toListItem(html) {
-    const li = document.createElement('li');
-    li.innerHTML = html;
-    return li;
-  }
-
   const vipareas = await fetchVIPAreas();
   const ul = document.createElement('ul');
 
-  vipareas.map(areaTemplate)
-    .map(toListItem)
+  vipareas.map(areaElement)
     .forEach((li) => ul.appendChild(li));
   block.append(ul);
 }
