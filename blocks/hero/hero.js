a9b54ce8e964d8534a5b68e1c67763820b717fb4
vdua <vdua@adobe.com>
2023-05-03 12:49:31 +0530

    feat: video in hero block

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,11 +3,17 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 export default async function decorate(block) {
   const heroContent = block.querySelector(':scope > div > div');
   const heroPic = block.querySelector(':scope picture');
+  const heroVideo = block.querySelector(':scope video');
   if (heroPic) {
     const img = heroPic.querySelector('img');
     heroPic.remove();
     const optimizedHeroPic = createOptimizedPicture(img.src, img.alt, false, [{ media: '(min-width: 600px)', width: '2000' }, { width: '1200' }]);
     block.append(optimizedHeroPic);
+  } else if (heroVideo) {
+    const videoParent = heroVideo.parentElement;
+    if (videoParent) {
+      block.append(videoParent);
+    }
   }
   if (heroContent) {
     if (heroContent.parentElement) {
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,6 +15,25 @@ import {
 
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
+function decorateVideo(block) {
+  const anchors = [...block.querySelectorAll('a[href]')];
+  const screens = ['desktop', 'mobile'];
+  const videos = anchors.map((a, i) => {
+    const video = document.createElement('video');
+    video.setAttribute('loop', '');
+    video.setAttribute('muted', '');
+    video.setAttribute('playsInline', '');
+    video.setAttribute('autoplay', '');
+    video.innerHTML = `<source src="${a.href}" type="video/mp4" />`;
+    if (screens[i]) {
+      video.setAttribute('data-screen', screens[i]);
+    }
+    return video;
+  });
+  block.innerHTML = '';
+  block.append(...videos);
+}
+
 /**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
@@ -22,11 +41,21 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+  const video = main.querySelector('.video');
+  const isPictureHero = h1 && picture
+    // eslint-disable-next-line no-bitwise
+    && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING);
+  const isVideoHero = h1 && video
+    // eslint-disable-next-line no-bitwise
+    && (h1.compareDocumentPosition(video) & Node.DOCUMENT_POSITION_PRECEDING);
+  if (isVideoHero) {
+    decorateVideo(video);
+  }
+  if (isPictureHero || isVideoHero) {
     const section = document.createElement('div');
-    const elems = [picture, h1];
-    const parents = [picture.parentElement];
+    const heroMedia = isPictureHero ? picture : video;
+    const elems = [heroMedia, h1];
+    const parents = [heroMedia.parentElement];
     const h2 = h1.nextElementSibling;
     let heading = h1;
     if (h2 && h2.tagName === 'H2') {
@@ -44,7 +73,11 @@ function buildHeroBlock(main) {
       elems.push(anchor);
       parents.push(anchor.parentElement);
     }
-    section.append(buildBlock('hero', { elems }));
+    const heroBlock = buildBlock('hero', { elems });
+    if (isVideoHero) {
+      heroBlock.classList.add('hero-video');
+    }
+    section.append(heroBlock);
     parents.filter((elem) => elem.tagName === 'P')
       .forEach((elem) => elem.remove());
     main.prepend(section);
