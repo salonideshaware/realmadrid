d51b387d997c9261dda731f48a3daece5d91c440
vdua <vdua@adobe.com>
2023-05-05 11:11:57 +0530

    feat: area-features with its dedicated block

---
+++ blocks/area-features/area-features.js
@@ -0,0 +1,58 @@
+import { buildBlock, decorateBlock, loadBlock } from '../../scripts/lib-franklin.js';
+
+function createDiv(...classNames) {
+  const div = document.createElement('div');
+  div.classList.add(...classNames);
+  return div;
+}
+
+function showPopup(e) {
+  const li = e.currentTarget;
+  const modalOverlay = document.getElementById('modal-overlay');
+  modalOverlay.classList.add('appear');
+  li.querySelector('.modal').classList.add('appear');
+}
+
+function makePopupCards(block) {
+  const items = block.querySelectorAll('li');
+  items.forEach((li) => {
+    const headingElements = li.querySelectorAll('span,h3');
+    const content = [...li.querySelectorAll('p')];
+    const modalDiv = createDiv('modal');
+    const modalContentDiv = createDiv('modal-content');
+    const header = createDiv('header');
+    header.append(...[...headingElements].map((h) => h.cloneNode(true)));
+    modalContentDiv.append(header, ...content);
+    modalDiv.append(modalContentDiv);
+    li.append(modalDiv);
+    modalDiv.addEventListener('click', (e) => {
+      e.currentTarget.classList.remove('appear');
+      const modalOverlay = document.getElementById('modal-overlay');
+      modalOverlay.classList.remove('appear');
+      e.stopPropagation();
+    });
+    modalContentDiv.addEventListener('click', (e) => {
+      e.stopPropagation();
+    });
+    li.addEventListener('click', showPopup);
+  });
+}
+
+export default async function decorate(block) {
+  const cardsBlock = buildBlock('cards', [...block.children].map((x) => [...x.children].map((y) => y.innerHTML)));
+  block.innerHTML = '';
+  block.append(cardsBlock);
+  decorateBlock(cardsBlock);
+  await loadBlock(cardsBlock);
+  const modalOverlay = document.getElementById('modal-overlay');
+  if (!modalOverlay) {
+    const div = document.createElement('div');
+    div.id = 'modal-overlay';
+    document.body.insertAdjacentElement('afterbegin', div);
+    div.addEventListener('click', () => {
+      modalOverlay.classList.remove('appear');
+      document.querySelector('modal.appear').classList.remove('appear');
+    });
+  }
+  makePopupCards(block);
+}
