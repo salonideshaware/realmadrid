cacc7dd9456c7eec4647246f42a4427778965f38
Emanuele Tomaello <etomaello@adobe.com>
2023-05-11 09:42:43 +0200

    feat: 42 - [tour] organize your visit - fix lint errors

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,3 +1,18 @@
+function decorateOrganizeVisit(el) {
+  [...el.children].forEach((row) => {
+    row.classList.add('visit-box');
+    if (row.children.length > 1) {
+      const rowElements = [...row.children];
+      rowElements[0].classList.add('icon-wrapper');
+      rowElements[1].classList.add('text-wrapper');
+      // remove buttons styling from stand-alone links
+      rowElements[1].querySelectorAll('.button, .button-container').forEach((button) => {
+        button.classList.remove('button', 'button-container', 'primary');
+      });
+    }
+  });
+}
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -20,19 +35,4 @@ export default function decorate(block) {
   if (block.classList.contains('organize-visit')) {
     decorateOrganizeVisit(block);
   }
-}
-
-function decorateOrganizeVisit(el) {
-  [...el.children].forEach((row) => {
-    row.classList.add('visit-box');
-    if (row.children.length > 1) {
-      const rowElements = [...row.children];
-      rowElements[0].classList.add('icon-wrapper');
-      rowElements[1].classList.add('text-wrapper');
-      // remove buttons styling from stand-alone links
-      for (const button of rowElements[1].querySelectorAll('.button, .button-container')) {
-        button.classList.remove('button', 'button-container', 'primary');
-      }
-    }
-  });
 }
