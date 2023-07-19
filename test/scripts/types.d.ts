24af25d28f943db010ce86065da045cb0f34d33e
Mirko Sagolj <37147400+msagolj@users.noreply.github.com>
2023-07-19 17:16:55 +0200

    remove test setup (#220)
    Remove boilerplate test setup as its not testing anything useful.
    
    Test URLs:
    - Before: https://main--realmadrid--hlxsites.hlx.live/sites/area-vip
    - After:
    https://remove-tests--realmadrid--hlxsites.hlx.live/sites/area-vip

--- test/scripts/types.d.ts
+++
@@ -1,5 +0,0 @@
-import type * as ScriptsModule from '../../sites/scripts/scripts';
-import type * as LibModule from '../../sites/scripts/lib-franklin';
-
-export type Scripts = typeof ScriptsModule;
-export type LibFranklin = typeof LibModule;
